5fcd76feb1309466d9bc91070c75716a8ffa6978
Navnit S <navnit_dav@yahoo.co.in>
2021-06-18 16:18:34 +0530

    initail setup for local dependencies

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './Scss/index.scss';
+
+const App = () => {
+  return <p>Everything is up and running!</p>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "main.js"
+    },
+    target: "node",
+    devServer: {
+        port: "3001",
+        contentBase: ["./public"],
+        open: true
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ]
+    }
+}
